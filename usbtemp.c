9081e2292676612f5b07f77c70f3f0bbe8d081e6
jaka <jaka@live.jp>
2017-06-08 17:46:22 +0200

    New error reporting model

--- usbtemp.c
+++ usbtemp.c
@@ -1,6 +1,6 @@
 /*
 
-    USB Thermometer CLI v1 Copyright 2017 jaka
+   USB Thermometer CLI v1 Copyright 2017 jaka
 
 */
 
@@ -20,14 +20,34 @@
 #define DS18X20_SP_SIZE 9
 #define DS18X20_GENERATOR 0x8c
 
+static char* ut_msgs[] = {
+  "",
+  "Error, could not get baud rate!",
+  "Error, could not set baud rate!",
+  "Error, serial port does not exist!",
+  "Error, you don't have rw permission to access serial port!",
+  "Error, failed to open serial port device!",
+  "Error, sensor not found!", /* 6 */
+  "Error, sensor CRC mismatch!",
+  "Warining, not expected sensor response!",
+};
+
+int ut_errno;
+
 /*
-   Signals taken from:
-   https://www.maximintegrated.com/en/app-notes/index.mvp/id/214
+  Signals taken from:
+  https://www.maximintegrated.com/en/app-notes/index.mvp/id/214
 */
+
+char *ut_errmsg(void)
+{
+  return ut_msgs[ut_errno];
+}
 
 static unsigned char lsb_crc8(unsigned char *data_in, unsigned int len, const unsigned char generator)
 {
-  unsigned char i, bit_counter, crc = 0;
+  unsigned char i, bit_counter;
+  unsigned char crc = 0;
 
   for (i = 0; i < len; i++) {
     crc ^= *(data_in + i);
@@ -35,7 +55,7 @@ static unsigned char lsb_crc8(unsigned char *data_in, unsigned int len, const un
     do {
       if (crc & 0x01)
         crc = (((crc >> 1) & 0x7f) ^ generator);
-      else
+     else
         crc = (crc >> 1) & 0x7f;
         bit_counter--;
     } while (bit_counter > 0);
@@ -43,225 +63,238 @@ static unsigned char lsb_crc8(unsigned char *data_in, unsigned int len, const un
   return crc;
 }
 
-static int file_exists(char *filename)
-{
-   struct stat st;
-
-   return (stat(filename, &st) == 0);
-}
-
 static int owReset(int fd)
 {
-   int rv;
-   unsigned char wbuff, rbuff;
-   fd_set readset;
-   struct timeval timeout_tv;
-   struct termios term;
+  int rv;
+  unsigned char wbuff, rbuff;
+  fd_set readset;
+  struct timeval timeout_tv;
+  struct termios term;
 
-   tcflush(fd, TCIOFLUSH);
+  tcflush(fd, TCIOFLUSH);
 
-   if (tcgetattr(fd, &term) < 0) {
-      fprintf(stderr, "Error, could not get baud rate!\n");
-      return -1;
-   }
-   term.c_cflag &= ~CSIZE | CS8;
-   cfsetispeed(&term, B9600);
-   cfsetospeed(&term, B9600);
-   tcsetattr(fd, TCSANOW, &term);
+  if (tcgetattr(fd, &term) < 0) {
+    ut_errno = 1;
+    return -1;
+  }
+  term.c_cflag &= ~CSIZE | CS8;
+  cfsetispeed(&term, B9600);
+  cfsetospeed(&term, B9600);
+  tcsetattr(fd, TCSANOW, &term);
 
-   /* Send the reset pulse. */
-   wbuff = 0xf0;
-   write(fd, &wbuff, 1);
+  /* Send the reset pulse. */
+  wbuff = 0xf0;
+  write(fd, &wbuff, 1);
 
-   timeout_tv.tv_usec = 0;
-   timeout_tv.tv_sec = TIMEOUT;
+  timeout_tv.tv_usec = 0;
+  timeout_tv.tv_sec = TIMEOUT;
 
-   FD_ZERO(&readset);
-   FD_SET(fd, &readset);
+  FD_ZERO(&readset);
+  FD_SET(fd, &readset);
 
-   if (select(fd + 1, &readset, NULL, NULL, &timeout_tv) > 0) {
+  if (select(fd + 1, &readset, NULL, NULL, &timeout_tv) > 0) {
 
-      if (FD_ISSET(fd, &readset)) {
-         read(fd, &rbuff, 1);
-         switch (rbuff) {
-            case 0:
-               /* Ground. */
-            case 0xf0:
-               /* No response. */
-               rv = -1; break;
-            default:
-               /* Got a response */
-               rv = 0;
-         }
+    if (FD_ISSET(fd, &readset)) {
+      read(fd, &rbuff, 1);
+      switch (rbuff) {
+        case 0:
+          /* Ground. */
+        case 0xf0:
+          /* No response. */
+          rv = -1; break;
+        default:
+          /* Got a response */
+          rv = 0;
       }
-      else {
-         rv = -1;
-      }
-   }
-   else
-      rv = -1; /* Timed out or interrupt. */
+    }
+    else {
+      rv = -1;
+    }
+  }
+  else
+    rv = -1; /* Timed out or interrupt. */
 
-   term.c_cflag &= ~CSIZE | CS6;
-   cfsetispeed(&term, B115200);
-   cfsetospeed(&term, B115200);
+  term.c_cflag &= ~CSIZE | CS6;
+  cfsetispeed(&term, B115200);
+  cfsetospeed(&term, B115200);
 
-   tcsetattr(fd, TCSANOW, &term);
+  tcsetattr(fd, TCSANOW, &term);
 
-   return rv;
+  return rv;
 }
 
 static unsigned char owWriteByte(int fd, unsigned char wbuff)
 {
-   char buf[8];
-   unsigned char rbuff, i;
-   size_t remaining, rbytes;
-   fd_set readset;
-   struct timeval timeout_tv;
+  char buf[8];
+  unsigned char rbuff, i;
+  size_t remaining, rbytes;
+  fd_set readset;
+  struct timeval timeout_tv;
 
-   tcflush(fd, TCIOFLUSH);
+  tcflush(fd, TCIOFLUSH);
 
-   for (i = 0; i < 8; i++)
-      buf[i] = (wbuff & (1 << (i & 0x7))) ? 0xff : 0x00;
-   write(fd, buf, 8);
+  for (i = 0; i < 8; i++)
+    buf[i] = (wbuff & (1 << (i & 0x7))) ? 0xff : 0x00;
+  write(fd, buf, 8);
 
-   timeout_tv.tv_usec = 0;
-   timeout_tv.tv_sec = 1;
+  timeout_tv.tv_usec = 0;
+  timeout_tv.tv_sec = 1;
 
-   FD_ZERO(&readset);
-   FD_SET(fd, &readset);
+  FD_ZERO(&readset);
+  FD_SET(fd, &readset);
 
-   rbuff = 0;
-   remaining = 8;
-   while (remaining > 0) {
+  rbuff = 0;
+  remaining = 8;
+  while (remaining > 0) {
 
-      if (select(fd + 1, &readset, NULL, NULL, &timeout_tv) > 0) {
+    if (select(fd + 1, &readset, NULL, NULL, &timeout_tv) > 0) {
 
-         if (FD_ISSET(fd, &readset)) {
-            rbytes = read(fd, &buf, remaining);
-            for (i = 0; i < rbytes; i++) {
-               rbuff >>= 1;
-               rbuff |= (buf[i] & 0x01) ? 0x80 : 0x00;
-               remaining--;
-            }
-         }
-         else
-            return 0xff;
+      if (FD_ISSET(fd, &readset)) {
+        rbytes = read(fd, &buf, remaining);
+        for (i = 0; i < rbytes; i++) {
+          rbuff >>= 1;
+          rbuff |= (buf[i] & 0x01) ? 0x80 : 0x00;
+          remaining--;
+        }
       }
       else
-         /* At last timeout will terminate while-loop. */
-         return 0xff;
-   }
-   return rbuff;
+        return 0xff;
+    }
+    else
+      /* At last timeout will terminate while-loop. */
+      return 0xff;
+  }
+  return rbuff;
 }
 
 static unsigned char owReadByte(int fd)
 {
-   return owWriteByte(fd, 0xff);
+  return owWriteByte(fd, 0xff);
 }
 
-void warn_owWriteByte(int fd, unsigned char wbuff)
+/* *********** */
+
+int warn_owWriteByte(int fd, unsigned char wbuff)
 {
-   if (owWriteByte(fd, wbuff) != wbuff)
-      fprintf(stderr, "Warining, not expected sensor response!\n");
+  if (owWriteByte(fd, wbuff) != wbuff)
+   return -1;
+  return 0;
 }
 
-int serialInit(char *serial_port)
+static int file_exists(char *filename)
 {
-   int fd;
-   struct termios term;
+  struct stat st;
+  return (stat(filename, &st) == 0);
+}
 
-   if (!file_exists(serial_port)) {
-      fprintf(stderr, "Error, serial port '%s' does not exist!\n", serial_port );
-      return -1;
-   }
+int serial_init(char *serial_port)
+{
+  int fd;
+  struct termios term;
 
-   if (access(serial_port, R_OK|W_OK) < 0) {
-      fprintf(stderr, "Error, you don't have rw permission to access serial port '%s'!\n", serial_port);
-      return -1;
-   }
+  if (!file_exists(serial_port)) {
+    ut_errno = 3;
+    return -1;
+  }
 
-   fd = open(serial_port, O_RDWR);
-   if (fd < 0) {
-      fprintf(stderr, "Error, failed to open serial port device!\n");
-      return -1;
-   }
+  if (access(serial_port, R_OK|W_OK) < 0) {
+    ut_errno = 4;
+    return -1;
+  }
 
-   memset(&term, 0, sizeof(term));
+  fd = open(serial_port, O_RDWR);
+  if (fd < 0) {
+    ut_errno = 5;
+    return -1;
+  }
 
-   term.c_cc[VMIN] = 1;
-   term.c_cc[VTIME] = 0;
-   term.c_cflag |= CS6 | CREAD | HUPCL | CLOCAL;
+  memset(&term, 0, sizeof(term));
 
-   cfsetispeed(&term, B115200);
-   cfsetospeed(&term, B115200);
+  term.c_cc[VMIN] = 1;
+  term.c_cc[VTIME] = 0;
+  term.c_cflag |= CS6 | CREAD | HUPCL | CLOCAL;
 
-   if (tcsetattr(fd, TCSANOW, &term) < 0) {
-      close(fd);
-      fprintf(stderr, "Error, could not set baud rate!\n");
-      return -1;
-   }
+  cfsetispeed(&term, B115200);
+  cfsetospeed(&term, B115200);
 
-   tcflush(fd, TCIOFLUSH);
+  if (tcsetattr(fd, TCSANOW, &term) < 0) {
+    close(fd);
+    ut_errno = 0;
+    return -1;
+  }
 
-   return fd;
+  tcflush(fd, TCIOFLUSH);
+
+  return fd;
 }
 
 int DS18B20_measure(int fd)
 {
-   if (owReset(fd) < 0) {
-      fprintf(stderr, "Error, no sensor found!\n");
-      return -1;
-   }
-   warn_owWriteByte(fd, 0xcc);
-   warn_owWriteByte(fd, 0x44);
-   return 0;
+  if (owReset(fd) < 0) {
+   ut_errno = 6;
+   return -1;
+  }
+  if (warn_owWriteByte(fd, 0xcc) < 0) {
+   ut_errno = 8;
+   return -1;
+  }
+  if (warn_owWriteByte(fd, 0x44) < 0) {
+   ut_errno = 8;
+   return -1;
+  }
+  return 0;
 }
 
 int DS18B20_acquire(int fd)
 {
-   time_t now;
-   struct tm *timeptr;
-   char timebuf[40];
-   unsigned short T;
-   float temperature;
-   unsigned char i, crc, sp_sensor[DS18X20_SP_SIZE];
+  time_t now;
+  struct tm *timeptr;
+  char timebuf[40];
+  unsigned short T;
+  float temperature;
+  unsigned char i, crc, sp_sensor[DS18X20_SP_SIZE];
 
-   if (owReset(fd) < 0) {
-      fprintf(stderr, "Error, no sensor found!\n");
-      return -1;
-   }
-   warn_owWriteByte(fd, 0xcc);
-   warn_owWriteByte(fd, 0xbe);
+  if (owReset(fd) < 0) {
+    ut_errno = 6;
+    return -1;
+  }
+  if (warn_owWriteByte(fd, 0xcc) < 0) {
+    ut_errno = 8;
+    return -1;
+  }
+  if (warn_owWriteByte(fd, 0xbe) < 0) {
+    ut_errno = 8;
+    return -1;
+  }
 
-   for (i = 0; i < DS18X20_SP_SIZE; i++)
-      sp_sensor[i] = owReadByte(fd);
+  for (i = 0; i < DS18X20_SP_SIZE; i++)
+    sp_sensor[i] = owReadByte(fd);
 
-   if ((sp_sensor[4] & 0x9f) != 0x1f) {
-      fprintf(stderr, "Error, sensor not found!\n");
-      return -1;
-   }
+  if ((sp_sensor[4] & 0x9f) != 0x1f) {
+    ut_errno = 6;
+    return -1;
+  }
 
-   crc = lsb_crc8(&sp_sensor[0], DS18X20_SP_SIZE - 1, 0x8c);
-   if (sp_sensor[DS18X20_SP_SIZE - 1] != crc) {
-      fprintf(stderr, "Error, sensor CRC mismatch!\n");
-      return -1;
-   }
+  crc = lsb_crc8(&sp_sensor[0], DS18X20_SP_SIZE - 1, 0x8c);
+  if (sp_sensor[DS18X20_SP_SIZE - 1] != crc) {
+    ut_errno = 7;
+    return -1;
+  }
 
-   T = (sp_sensor[1] << 8) + (sp_sensor[0] & 0xff);
+  T = (sp_sensor[1] << 8) + (sp_sensor[0] & 0xff);
+  if ((T >> 15) & 0x01) {
+    T--;
+    T ^= 0xffff;
+    T *= -1;
+  }
+  temperature = (float)T / 16;
 
-   if ((T >> 15) & 0x01) {
-      T--;
-      T ^= 0xffff;
-      T *= -1;
-   }
-   temperature = (float)T / 16;
+  time(&now);
+  timeptr = localtime(&now);
+  strftime(timebuf, sizeof(timebuf), "%b %d %H:%M:%S", timeptr);
 
-   time(&now);
-   timeptr = localtime(&now);
-   strftime(timebuf, sizeof(timebuf), "%b %d %H:%M:%S", timeptr);
-
-   printf("%s Sensor C: %.2f\n", timebuf, temperature);
-   return 0;
+  printf("%s Sensor C: %.2f\n", timebuf, temperature);
+  return 0;
 }
 
 #define OPTS_HELP 1
@@ -269,68 +302,75 @@ int DS18B20_acquire(int fd)
 
 int main(int argc, char **argv)
 {
-   char *serial_port = NULL;
-   struct timeval wait_tv;
-   char c;
-   int fd;
-   int opts = OPTS_VERBOSE, rv = 0;
+  struct timeval wait_tv;
+  char c;
+  int fd;
+  char *serial_port = NULL;
+  int opts = OPTS_VERBOSE;
+  int rv = 0;
 
-   while ((c = getopt(argc, argv, "hqs:")) != -1) {
-      switch (c) {
-         case 'h':
-            opts |= OPTS_HELP;
-            break;
-         case 'q':
-            opts &= ~OPTS_VERBOSE;
-            break;
-         case 's':
-            serial_port = optarg;
-            break;
-         case '?':
-            return -1;
-      }
-   }
+  while ((c = getopt(argc, argv, "hqs:")) != -1) {
+    switch (c) {
+      case 'h':
+        opts |= OPTS_HELP;
+        break;
+      case 'q':
+        opts &= ~OPTS_VERBOSE;
+        break;
+      case 's':
+        serial_port = optarg;
+        break;
+      case '?':
+        return -1;
+    }
+  }
 
-   if (opts & OPTS_HELP)
-      opts |= OPTS_VERBOSE;
+  if (opts & OPTS_HELP)
+    opts |= OPTS_VERBOSE;
 
-   if (opts & OPTS_VERBOSE)
-      printf("USB Thermometer CLI v1 Copyright 2017 jaka\n");
+  if (opts & OPTS_VERBOSE)
+    printf("USB Thermometer CLI v1 Copyright 2017 jaka\n");
 
-   if (opts & OPTS_HELP) {
-      printf("\t-q\tQuiet mode\n");
-      printf("\t-s\tSet serial port\n");
-      return 0;
-   }
+  if (opts & OPTS_HELP) {
+    printf("\t-q\tQuiet mode\n");
+    printf("\t-s\tSet serial port\n");
+    return 0;
+  }
 
-   if (!serial_port)
-      serial_port = "/dev/ttyUSB0";
-   if (opts & OPTS_VERBOSE)
-      printf("Using serial port: %s\n", serial_port);
+  if (!serial_port)
+    serial_port = "/dev/ttyUSB0";
+  if (opts & OPTS_VERBOSE)
+    printf("Using serial port: %s\n", serial_port);
 
-   fd = serialInit(serial_port);
-   if (fd > 0) {
+  fd = serial_init(serial_port);
+  if (fd > 0) {
 
-      if (DS18B20_measure(fd) < 0) {
-         close(fd);
-         return -1;
-      }
-
-      wait_tv.tv_usec = 0;
-      wait_tv.tv_sec = 1;
-
-      if (opts & OPTS_VERBOSE)
-         printf("Waiting for response ...\n");
-
-      select(0, NULL, NULL, NULL, &wait_tv);
-
-      if (DS18B20_acquire(fd) < 0)
-         rv = -1;
-
+    if (DS18B20_measure(fd) < 0) {
+      fprintf(stderr, "%s\n", ut_errmsg());
       close(fd);
-      return rv;
+      return -1;
+    }
 
-   }
+    wait_tv.tv_usec = 0;
+    wait_tv.tv_sec = 1;
 
-   return -1;
+    if (opts & OPTS_VERBOSE)
+      printf("Waiting for response ...\n");
+
+    select(0, NULL, NULL, NULL, &wait_tv);
+
+    if (DS18B20_acquire(fd) < 0) {
+      fprintf(stderr, "%s\n", ut_errmsg());
+      rv = -1;
+    }
+
+    close(fd);
+    return rv;
+
+  }
+  else {
+    fprintf(stderr, "%s\n", ut_errmsg());
+  }
+
+  return -1;
 }
